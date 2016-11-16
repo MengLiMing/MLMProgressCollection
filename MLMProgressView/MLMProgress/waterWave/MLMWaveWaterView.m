0bc792f73dd78caadcf436aa52903613fa33e413
MengLiMing <920459250@qq.com>
2016-11-16 11:40:39 +0800

    update draw method

--- MLMProgressView/MLMProgress/waterWave/MLMWaveWaterView.m
+++ MLMProgressView/MLMProgress/waterWave/MLMWaveWaterView.m
@@ -11,9 +11,6 @@
 
 @interface MLMWaveWaterView ()
 {
-    CAShapeLayer *topLayer;
-    CAShapeLayer *bottomLayer;
-
     CGFloat _wave_offsety;//根据进度计算(波峰所在位置的y坐标)
     CGFloat _offsety_scale;//上升的速度
     
@@ -35,14 +32,12 @@
         self.clipsToBounds = YES;
         [self initView];
     }
-
-    [self drowWave];
+    
     return self;
 }
 
 #pragma mark - initView
 - (void)initView {
-
     
     _wave_Amplitude = self.frame.size.width/20;
     _wave_Cycle = 2*M_PI/(self.frame.size.width * .9);
@@ -64,15 +59,16 @@
     [self startWave];
 }
 
-#pragma mark - animation
-- (void)changeoff {
-    _wave_offsetx += _wave_move_width*_wave_scale;
-    [self drowWave];
+
+#pragma mark - drawRect
+- (void)drawRect:(CGRect)rect {
+    [self drawWave:0 withColor:_topColor];
+    [self drawWave:_wave_distance withColor:_bottomColor];
 }
 
-#pragma mark - draw layer
-- (void)drawLayer:(CAShapeLayer *)layer offsetY:(CGFloat)offsetY {
-    
+
+#pragma mark - draw wave
+- (void)drawWave:(CGFloat)offsetY withColor:(UIColor*)color {
     //波浪动画，所以进度的实际操作范围是，多加上两个振幅的高度,到达设置进度的位置y坐标
     CGFloat end_offY = (1-_progress) * (self.frame.size.height + 2* _wave_Amplitude);
     if (_progress_animation) {
@@ -86,56 +82,50 @@
     } else {
         _wave_offsety = end_offY;
     }
-
-    CGMutablePathRef path = CGPathCreateMutable();
+    
+    UIBezierPath *wave = [UIBezierPath bezierPath];
     for (float next_x= 0.f; next_x <= self.frame.size.width; next_x ++) {
         //正弦函数
         CGFloat next_y = _wave_Amplitude * sin(_wave_Cycle*next_x + _wave_offsetx + offsetY/200*2*M_PI) + _wave_offsety;
         if (next_x == 0) {
-            CGPathMoveToPoint(path, nil, next_x, next_y - _wave_Amplitude);
+            [wave moveToPoint:CGPointMake(next_x, next_y - _wave_Amplitude)];
         } else {
-            CGPathAddLineToPoint(path, nil, next_x, next_y- _wave_Amplitude);
+            [wave addLineToPoint:CGPointMake(next_x, next_y - _wave_Amplitude)];
         }
     }
-    
-    CGPathAddLineToPoint(path, nil, self.frame.size.width, self.frame.size.height);
-    CGPathAddLineToPoint(path, nil, 0, self.frame.size.height);
-    CGPathCloseSubpath(path);
-    layer.path = path;
-    CGPathRelease(path);
+    [wave addLineToPoint:CGPointMake(self.frame.size.width, self.frame.size.height)];
+    [wave addLineToPoint:CGPointMake(0, self.bounds.size.height)];
+    [color set];
+    [wave fill];
 }
 
-#pragma mark - after setter
-- (void)drowWave {
-    topLayer.fillColor = _topColor.CGColor;
-    bottomLayer.fillColor = _bottomColor.CGColor;
-    
-    [self drawLayer:topLayer offsetY:0];
-    [self drawLayer:bottomLayer offsetY:_wave_distance];
-    
+#pragma mark - animation
+- (void)changeoff {
+    _wave_offsetx += _wave_move_width*_wave_scale;
+    [self setNeedsDisplay];
 }
-
 
 
 #pragma mark - setter
 - (void)setProgress:(CGFloat)progress {
     _progress = progress;
-    [self drowWave];
+    [self setNeedsDisplay];
+    
 }
 
 - (void)setWave_Amplitude:(CGFloat)wave_Amplitude {
     _wave_Amplitude = wave_Amplitude;
-    [self drowWave];
+    [self setNeedsDisplay];
 }
 
 - (void)setWave_Cycle:(CGFloat)wave_Cycle {
     _wave_Cycle = wave_Cycle;
-    [self drowWave];
+    [self setNeedsDisplay];
 }
 
 - (void)setWave_distance:(CGFloat)wave_distance {
     _wave_distance = wave_distance;
-    [self drawLayer:bottomLayer offsetY:_wave_distance];
+    [self setNeedsDisplay];
 }
 
 - (void)setWave_scale:(CGFloat)wave_scale {
@@ -144,15 +134,6 @@
 
 #pragma mark - reStart
 - (void)startWave {
-    if (!bottomLayer) {
-        bottomLayer = [[CAShapeLayer alloc] init];
-        [self.layer addSublayer:bottomLayer];
-    }
-    
-    if (!topLayer) {
-        topLayer = [[CAShapeLayer alloc] init];
-        [self.layer addSublayer:topLayer];
-    }
     
     if (!_waveDisplaylink) {
         _waveDisplaylink = [CADisplayLink displayLinkWithTarget:self selector:@selector(changeoff)];
@@ -166,14 +147,6 @@
         _waveDisplaylink = nil;
     }
     
-    if (topLayer) {
-        [topLayer removeFromSuperlayer];
-        topLayer = nil;
-    }
-    if (bottomLayer) {
-        [bottomLayer removeFromSuperlayer];
-        bottomLayer = nil;
-    }
 }
 
 @end
